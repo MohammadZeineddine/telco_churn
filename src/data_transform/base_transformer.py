e5a46d8aaa16458dfc214d1ab76b0da782d10cea
MohammadZeineddine <99921709+MohammadZeineddine@users.noreply.github.com>
2024-12-14 14:21:52 +0200

    Rename DataTransformer class to BaseTransformer for clarity

--- src/data_transform/base_transformer.py
+++ src/data_transform/base_transformer.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 import pandas as pd
 
 
-class DataTransformer(ABC):
+class BaseTransformer(ABC):
     @abstractmethod
     def transform(self, data: pd.DataFrame) -> pd.DataFrame:
         """Apply transformation to the data."""
